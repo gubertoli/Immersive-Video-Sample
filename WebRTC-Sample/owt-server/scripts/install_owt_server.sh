81413c168d62a5736f746dac7bc84700bc3ef49e
Gustavo Bertoli <gustavo.bertoli@airbus.com>
2025-10-29 15:38:50 +0100

    fixing other EOL issues

--- WebRTC-Sample/owt-server/scripts/install_owt_server.sh
+++ WebRTC-Sample/owt-server/scripts/install_owt_server.sh
@@ -18,9 +18,10 @@ install_devtoolset() {
     sudo yum install -y centos-release-scl
     echo "Applying EOL fix to SCLo repositories..."
     pushd /etc/yum.repos.d/ > /dev/null
-    sudo sed -i 's/mirrorlist/#mirrorlist/g' CentOS-SCLo-scl.repo CentOS-SCLo-scl-rh.repo
-    sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' CentOS-SCLo-scl.repo CentOS-SCLo-scl-rh.repo
+    sudo sed -i 's/mirrorlist/#mirrorlist/g' *.repo
+    sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' *.repo
     popd > /dev/null
+    sudo yum clean metadata
     sudo yum clean all
     echo "EOL fix applied."
     sudo yum install -y devtoolset-7
